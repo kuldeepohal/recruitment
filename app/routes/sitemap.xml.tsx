99d41a41105e2c428411e1106f6d5c6b918287a8
kuldeep Ohal <34094859+kuldeepohal@users.noreply.github.com>
2026-08-26 15:21:05 +0530

    Await database initialization for sitemap generation

--- app/routes/sitemap.xml.tsx
+++ app/routes/sitemap.xml.tsx
@@ -4,25 +4,10 @@ import { getPublishedSlugs } from "../models/recruitment.server";
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const origin = (process.env.SITE_URL || new URL(request.url).origin).replace(/\/$/, "");
-  const slugs = await getPublishedSlugs(createDb());
-  const urls = [
-    `${origin}/`,
-    `${origin}/jobs`,
-    ...slugs.map((slug) => `${origin}/recruitment/${encodeURIComponent(slug)}`),
-  ];
-
-  const body = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls
-    .map((url) => `<url><loc>${escapeXml(url)}</loc></url>`)
-    .join("")}\n</urlset>`;
-
-  return new Response(body, {
-    headers: {
-      "Content-Type": "application/xml; charset=utf-8",
-      "Cache-Control": "public, max-age=3600",
-    },
-  });
+  const db = await createDb();
+  const slugs = await getPublishedSlugs(db);
+  const urls = [`${origin}/`, `${origin}/jobs`, ...slugs.map((slug) => `${origin}/recruitment/${encodeURIComponent(slug)}`)];
+  const body = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls.map((url) => `<url><loc>${escapeXml(url)}</loc></url>`).join("")}\n</urlset>`;
+  return new Response(body, { headers: { "Content-Type": "application/xml; charset=utf-8", "Cache-Control": "public, max-age=3600" } });
 }
-
-function escapeXml(value: string) {
-  return value.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;");
-}
+function escapeXml(value: string) { return value.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;"); }
